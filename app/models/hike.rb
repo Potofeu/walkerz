c7bc4ff15935ae0087f398b7298f16793137cfa8
Hugo Flamigni <hugoflam@gmail.com>
2023-06-17 20:00:40 +0200

    Adding associations in DB models

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,3 +1,10 @@
 class Hike < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+  has_many :favorites
+  has_many :users, through: :favorites
+  has_many :points_of_interests
+  has_many :locations, through: :points_of_interests
+  has_many :hikes_categories
+  has_many :categories, through: :hikes_categories
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+  has_many :points_of_interests
+  has_may :hikes, through: :points_of_interests
 end
