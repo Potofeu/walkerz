dfcbcc6035de3fd6f7e772e7e00efd7cf068eb62
Hugo Flamigni <hugoflam@gmail.com>
2023-06-17 20:11:10 +0200

    Setting up pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit # ajoute les policies à toues les controllers
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
