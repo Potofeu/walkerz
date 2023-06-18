4bd0f9d2786275f0bf81f7baddd00aafa4a9bc8b
Hugo Flamigni <hugoflam@gmail.com>
2023-06-18 15:16:41 +0200

    migrating the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,76 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_152909) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_17_171729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "hike_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hike_id"], name: "index_favorites_on_hike_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "hikes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "time"
+    t.float "distance"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_hikes_on_user_id"
+  end
+
+  create_table "hikes_categories", force: :cascade do |t|
+    t.bigint "hike_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_hikes_categories_on_category_id"
+    t.index ["hike_id"], name: "index_hikes_categories_on_hike_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.float "latitude"
+    t.float "longitude"
+    t.text "description"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "points_of_interests", force: :cascade do |t|
+    t.bigint "hike_id", null: false
+    t.bigint "location_id", null: false
+    t.integer "step"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hike_id"], name: "index_points_of_interests_on_hike_id"
+    t.index ["location_id"], name: "index_points_of_interests_on_location_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "hike_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hike_id"], name: "index_reviews_on_hike_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +89,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_152909) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "hikes"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "hikes", "users"
+  add_foreign_key "hikes_categories", "categories"
+  add_foreign_key "hikes_categories", "hikes"
+  add_foreign_key "points_of_interests", "hikes"
+  add_foreign_key "points_of_interests", "locations"
+  add_foreign_key "reviews", "hikes"
+  add_foreign_key "reviews", "users"
 end
