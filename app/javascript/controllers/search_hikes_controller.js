5078e5c7e90deaa46423bd9a06ab254203eace3f
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-06-21 21:10:56 +0200

    factorisation du code du controller

--- app/javascript/controllers/search_hikes_controller.js
+++ app/javascript/controllers/search_hikes_controller.js
@@ -14,31 +14,18 @@ export default class extends Controller {
     event.preventDefault();
     const query = event.currentTarget.value;
     const url = `${this.formTarget.action}?query=${query}`;
+    this.recherche(url);
 
-    fetch(url,{
-      method: "GET",
-      headers: {
-        'Accept': 'application/json'
-      }
-    })
-    .then(response => response.json())
-    .then((data) => {
-      // console.log(data)
-      // console.log(data.listes)
-      this.listTarget.outerHTML = data.listes;
-    })
   }
   update_with_category(event){
     event.preventDefault();
-    console.log("lien:",event.currentTarget.href);
-    // On a donc plusieurs liens potentiels.
-    // Il faut donc préciser qu'il s'agit de la target de l'évènement qui a été cliqué
-    // pour ça, on dit : event.target.href ou event.currentTarget.href
+    // console.log("lien:",event.currentTarget.href);
     const url = `${event.target.href}`;
-    // const url = `${event.currentTarget.href}`;
 
-    // console.log("cont url", url);
+    this.recherche(url);
+  }
 
+  recherche(url){
     fetch(url,{
       method: "GET",
       headers: {
