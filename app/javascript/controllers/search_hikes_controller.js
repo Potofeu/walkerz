909a942692d34d93b7cb18036e1a23214cc865d1
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-06-21 12:41:48 +0200

    ajout contrller js et modif vue

---
+++ app/javascript/controllers/search_hikes_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="search-hikes"
+export default class extends Controller {
+  static targets = ["form", "list", 'input']
+  connect() {
+    // console.log("hello du controller")
+    // console.log(this.formTarget)
+    // console.log(this.listTarget)
+    // console.log(this.inputTarget)
+  }
+
+  update(event){
+    event.preventDefault();
+    const query = event.currentTarget.value;
+    const url = `${this.formTarget.action}?query=${query}`;
+
+    fetch(url,{
+      method: "GET",
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((data) => {
+      // console.log(data)
+      this.listTarget.outerHTML = data;
+    })
+  }
+}
---
+++ app/views/hikes/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @hikes, partial: 'hike', as: :hike
