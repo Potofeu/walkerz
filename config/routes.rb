bb6c1e7efdcf72c2a4956b0d531d4e72a430112a
Mickael Souchet <souchet.mickael@gmail.com>
2023-06-27 22:12:28 +0200

    sign out fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session, via: [:get, :delete]
-  end
+  devise_for :users
   root to: "hikes#index"
 
   resources :favorites, only: [:index]
