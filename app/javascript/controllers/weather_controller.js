31bfe38591f28d13d3a71b279b45d22163915a80
Mickael Souchet <souchet.mickael@gmail.com>
2023-07-01 11:06:17 +0200

    Weathercall fonctionnel, CSS adapté, titre h1 centré

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -2,22 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="weather"
 export default class extends Controller {
-  static targets = ["icon", "temperature"]
-
   static values = { apiKey: String }
 
+  static targets = ["icon", "temperature", "city"]
+
   connect() {
-    console.log(this.element.value);
+    console.log(this.cityTarget.attributes.city.value);
+    console.log(this.cityTarget.attributes.api.value);
+    console.log(this.temperatureTarget);
 
-  //   const city = this.element.value
-  //   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`)
-  //     .then(response => response.json())
-  //     .then(data => this.#updateCard(data))
-  // }
+    const city = this.cityTarget.attributes.city.value
+    const apikey = this.cityTarget.attributes.api.value
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
+      .then(response => response.json())
+      .then(data => this.#updateCard(data))
+  }
 
-  // #updateCard(data) {
-  //   this.iconTarget.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-  //   this.temperatureTarget.innerText = `${Math.round(data.main.temp)} °C`
-  // }
+  #updateCard(data) {
+    this.iconTarget.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    this.temperatureTarget.innerText = `${Math.round(data.main.temp)} °C`
   }
 }
