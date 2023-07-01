9132d961e0cb3d342c89862e097664afb88cdf34
Mickael Souchet <souchet.mickael@gmail.com>
2023-07-01 09:48:39 +0200

    master merged

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -2,9 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="weather"
 export default class extends Controller {
-  static targets = ["icon"]
+  static targets = ["icon", "temperature"]
+
+  static values = { apiKey: String }
 
   connect() {
-    console.log("hello from weather JS")
+    console.log(this.element.value);
+
+  //   const city = this.element.value
+  //   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`)
+  //     .then(response => response.json())
+  //     .then(data => this.#updateCard(data))
+  // }
+
+  // #updateCard(data) {
+  //   this.iconTarget.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+  //   this.temperatureTarget.innerText = `${Math.round(data.main.temp)} °C`
+  // }
   }
 }
