b20e10e8a7a848e658f9d58326856bbae404f1c5
Hugo Flamigni <hugoflam@gmail.com>
2023-07-12 18:36:48 +0200

    removing name field and adding description in location creation

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -17,11 +17,19 @@ class LocationsController < ApplicationController
 
     if Location.exists?(address: @location.address)
       @existing_location = Location.find_by address: @location.address
-      PointsOfInterest.create!(hike_id: @hike.id, location_id: @existing_location.id)
+      PointsOfInterest.create!(
+        hike_id: @hike.id,
+        location_id: @existing_location.id,
+        description: @existing_location.description
+      )
       redirect_to new_hike_location_path(@hike)
     else
       if @location.save!
-        PointsOfInterest.create!(hike_id: @hike.id, location_id: @location.id)
+        PointsOfInterest.create!(
+          hike_id: @hike.id,
+          location_id: @location.id,
+          description: @location.description
+        )
         redirect_to new_hike_location_path(@hike)
       else
         render :new, statut: :unprocessable_entity
